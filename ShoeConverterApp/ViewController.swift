01bc0d0ec1b74e24c432c30ec30b8c6961fde075
COConsulting <info@newconsultant.us>
2015-02-06 16:29:59 -0700

    Initial Commit

---
+++ ShoeConverterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeConverterApp
+//
+//  Created by Development on 2/6/15.
+//  Copyright (c) 2015 COC Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
